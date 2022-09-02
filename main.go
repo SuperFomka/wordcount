17722fccfbd9d610bfa2d4b3fc0024ee677ef2bf
SuperFomka <fomka1907@gmail.com>
2022-09-02 13:51:45 +0300

    third commit

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"bufio"
+	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	rd := bufio.NewReader(os.Stdin)
-	str, _ := rd.ReadString('\n')
+	pattern, str, err := readInput()
+	if err != nil {
+		fail(err)
+	}
+	isMatch := match(pattern, str)
+	if !isMatch {
+		os.Exit(0)
+	}
 	words := strings.Split(str, " ")
 	fmt.Println(len(words))
+}
 
+func readInput() (pattern, src string, err error) {
+	flag.StringVar(&pattern, "p", "", "pattern to match against")
+	flag.Parse()
+	if pattern == "" {
+		return pattern, src, errors.New("missing pattern")
+	}
+	src = strings.Join(flag.Args(), "")
+	if src == "" {
+		return pattern, src, errors.New("missing string to match")
+	}
+	return pattern, src, nil
+}
+
+func fail(err error) {
+	fmt.Println("match:", err)
+	os.Exit(1)
+}
+
+func match(pattern string, src string) bool {
+	return strings.Contains(src, pattern)
 }
